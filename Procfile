2f966fc1956b887745c9c1f51ccdec2f0a14de0e
Andres Cusirramos Marquez Mares <andres.cusirramos@ucsp.edu.pe>
2022-09-16 18:40:58 -0500

    o

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn main:app --log-file=-
+web: gunicorn main.py:application --log-file - --log-level debug
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 #start the server 
 from website import create_app
 
+
+
 app = create_app()
 
 if __name__ == '__main__':
