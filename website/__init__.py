405d60261657d50f52faa0cb7a95a0d877798d89
Andres Cusirramos Marquez Mares <andres.cusirramos@ucsp.edu.pe>
2022-09-16 23:06:14 -0500

    add gif

--- website/__init__.py
+++ website/__init__.py
@@ -11,7 +11,6 @@ DB_NAME = "database.db"
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'hjshjhdjah kjshkjdhjs'
-    #app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://xmqjdsghbhfbrd:c4f86ce4bf89cc56f0691cb56df62c47c3da2778e222291ccae850ccc9302d12@ec2-3-225-110-188.compute-1.amazonaws.com:5432/d25oaatvqq2od2'
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     db.init_app(app)
 
